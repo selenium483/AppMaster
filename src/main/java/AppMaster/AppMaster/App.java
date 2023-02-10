efd90a66be622f00845d68e874648c46aadd0e62
kumarankesh483 <kumarankesh483@gmail.com>
2023-02-10 19:53:21 +0530

    first commit

---
+++ src/main/java/AppMaster/AppMaster/App.java
@@ -0,0 +1,13 @@
+package AppMaster.AppMaster;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
